630815170678d360b56365fbd91870b8d05e2d89
Gustavo GIUNCO BERTOLDI <gg.bertoldi@hotmail.com>
2022-06-23 15:05:29 +0200

    API url

--- back/app.js
+++ back/app.js
@@ -17,7 +17,7 @@ let app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", `http://${process.env.CORS_ALLOW}`);
+  res.header("Access-Control-Allow-Origin", `${process.env.CORS_ALLOW}`);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
