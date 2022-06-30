21802f0763fd799e3834cc00839a683a32fd938a
Gustavo GIUNCO BERTOLDI <gg.bertoldi@hotmail.com>
2022-06-30 11:40:28 +0200

    Code cleanup

--- front/.svelte-kit/output/server/entries/fallbacks/layout.svelte.js
+++
@@ -1,5 +0,0 @@
-import { c as create_ssr_component } from "../../chunks/index-e3c45e72.js";
-const Layout = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `${slots.default ? slots.default({}) : ``}`;
-});
-export { Layout as default };
