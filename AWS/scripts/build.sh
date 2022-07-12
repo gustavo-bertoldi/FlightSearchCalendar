4fda635060c51c17502a8473845b1ea7e6eb5000
Gustavo GIUNCO BERTOLDI <gg.bertoldi@hotmail.com>
2022-07-12 11:46:28 +0200

    Fixed build script

--- AWS/scripts/build.sh
+++ AWS/scripts/build.sh
@@ -1,1 +1,1 @@
-docker build -t flight-search-app --build-arg PORT=$PORT --no-cache=true .
+docker build -t flight-search-app --build-arg PORT=$PORT --no-cache=true /home/ec2-user/FlightSearchCalendar
