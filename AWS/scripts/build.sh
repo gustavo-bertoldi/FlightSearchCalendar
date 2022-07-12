7c5edcafaae1358ffe7493f94e22d05b368b7f08
Gustavo GIUNCO BERTOLDI <gg.bertoldi@hotmail.com>
2022-07-12 11:17:42 +0200

    Minor fix for code deploy

---
+++ AWS/scripts/build.sh
@@ -0,0 +1,1 @@
+docker build -t flight-search-app --build-arg PORT=$PORT --no-cache=true /home/ec2-user/FlightSearchCalendar
--- AWS/scripts/stop.sh
+++
@@ -1,2 +0,0 @@
-docker stop flight-search-app
-docker rm flight-search-app
