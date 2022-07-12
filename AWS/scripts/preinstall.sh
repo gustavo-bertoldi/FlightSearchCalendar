1e0f34d359a4f77b7417779152363de33e33b4d4
Gustavo GIUNCO BERTOLDI <gg.bertoldi@hotmail.com>
2022-07-12 14:54:02 +0200

    Secrets retrieve in AWS

--- AWS/scripts/preinstall.sh
+++ AWS/scripts/preinstall.sh
@@ -1,7 +1,7 @@
 mkdir -p /home/ec2-user/FlightSearchApp
-export AMADEUS_ENV=test
-export PORT=3000
-export AMADEUS_CLIENT_ID=$(aws secretsmanager get-secret-value --region eu-west-1 --secret-id FlightSearchApp/AMADEUS_CLIENT_ID --query SecretString --output text | jq -r .AMADEUS_CLIENT_ID)
-export AMADEUS_CLIENT_SECRET=$(aws secretsmanager get-secret-value --region eu-west-1 --secret-id FlightSearchApp/AMADEUS_CLIENT_SECRET --query SecretString --output text | jq -r .AMADEUS_CLIENT_SECRET)
+AMADEUS_ENV=test
+PORT=3000
+AMADEUS_CLIENT_ID=$(aws secretsmanager get-secret-value --region eu-west-1 --secret-id FlightSearchApp/AMADEUS_CLIENT_ID --query SecretString --output text | jq -r .AMADEUS_CLIENT_ID)
+AMADEUS_CLIENT_SECRET=$(aws secretsmanager get-secret-value --region eu-west-1 --secret-id FlightSearchApp/AMADEUS_CLIENT_SECRET --query SecretString --output text | jq -r .AMADEUS_CLIENT_SECRET)
 
 
