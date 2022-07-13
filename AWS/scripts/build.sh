25ee40e8ba1fd7d7f08d421f0c6a334c53230eda
Gustavo GIUNCO BERTOLDI <gg.bertoldi@hotmail.com>
2022-07-13 09:00:39 +0200

    Fix deploy scripts

--- AWS/scripts/build.sh
+++ AWS/scripts/build.sh
@@ -1,1 +1,5 @@
+AMADEUS_ENV=test
+PORT=3000
+AMADEUS_CLIENT_ID=$(aws secretsmanager get-secret-value --region eu-west-1 --secret-id FlightSearchApp/AMADEUS_CLIENT_ID --query SecretString --output text | jq -r .AMADEUS_CLIENT_ID)
+AMADEUS_CLIENT_SECRET=$(aws secretsmanager get-secret-value --region eu-west-1 --secret-id FlightSearchApp/AMADEUS_CLIENT_SECRET --query SecretString --output text | jq -r .AMADEUS_CLIENT_SECRET)
 docker build -t flight-search-app --build-arg PORT=$PORT --no-cache=true /home/ec2-user/FlightSearchApp
--- AWS/scripts/preinstall.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p /home/ec2-user/FlightSearchApp
-AMADEUS_ENV=test
-PORT=3000
-AMADEUS_CLIENT_ID=$(aws secretsmanager get-secret-value --region eu-west-1 --secret-id FlightSearchApp/AMADEUS_CLIENT_ID --query SecretString --output text | jq -r .AMADEUS_CLIENT_ID)
-AMADEUS_CLIENT_SECRET=$(aws secretsmanager get-secret-value --region eu-west-1 --secret-id FlightSearchApp/AMADEUS_CLIENT_SECRET --query SecretString --output text | jq -r .AMADEUS_CLIENT_SECRET)
-
-
