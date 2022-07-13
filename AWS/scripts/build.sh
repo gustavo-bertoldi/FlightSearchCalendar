54f98eea07ff5fe1d0d606f3d869adc1b359a07a
Gustavo GIUNCO BERTOLDI <gg.bertoldi@hotmail.com>
2022-07-13 16:27:04 +0200

    New scripst

--- AWS/scripts/build.sh
+++ AWS/scripts/build.sh
@@ -1,1 +1,1 @@
-docker build -t flight-search-app --build-arg PORT=3000 --build-arg EC2_INSTANCE_IP=54.75.219.41 /home/ec2-user/FlightSearchApp
+docker build -t flight-search-app --build-arg PORT=3000 --no-cache=true /home/ec2-user/FlightSearchApp
