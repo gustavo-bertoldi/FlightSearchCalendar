654a218471d8b4b1a612389aee4c51a61096f38a
Gustavo GIUNCO BERTOLDI <gg.bertoldi@hotmail.com>
2022-07-13 09:21:06 +0200

    Fix build script

--- AWS/scripts/start.sh
+++ AWS/scripts/start.sh
@@ -1,1 +1,5 @@
+export AMADEUS_ENV=test
+export PORT=3000
+export AMADEUS_CLIENT_ID=$(aws secretsmanager get-secret-value --region eu-west-1 --secret-id FlightSearchApp/AMADEUS_CLIENT_ID --query SecretString --output text | jq -r .AMADEUS_CLIENT_ID)
+export AMADEUS_CLIENT_SECRET=$(aws secretsmanager get-secret-value --region eu-west-1 --secret-id FlightSearchApp/AMADEUS_CLIENT_SECRET --query SecretString --output text | jq -r .AMADEUS_CLIENT_SECRET)
 docker run -d -e AMADEUS_CLIENT_ID -e AMADEUS_CLIENT_SECRET -e AMADEUS_ENV -p $PORT:$PORT --name flight-search-app flight-search-app
