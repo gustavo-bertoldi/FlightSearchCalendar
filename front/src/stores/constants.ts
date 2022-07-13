28c7e7330cb3c1d2fb0bf644fe5be093a7933bbe
Gustavo GIUNCO BERTOLDI <gg.bertoldi@hotmail.com>
2022-07-13 10:01:16 +0200

    Build fix

--- front/src/stores/constants.ts
+++ front/src/stores/constants.ts
@@ -2,5 +2,5 @@ import { readable, type Readable } from 'svelte/store';
 
 export const CONSTANTS: Readable<{ [index: string]: string }> = readable({
 	AMADEUS_BLUE: 'rgb(0,94,184)',
-	API_URL: 'http://localhost:3000'
+	API_URL: 'http://54.75.219.41'
 });
