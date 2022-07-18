0be65cb84653ba453ffbfc4b3f70aff3e6112312
Gustavo GIUNCO BERTOLDI <gg.bertoldi@hotmail.com>
2022-07-18 16:26:22 +0200

    Fixed back addres

--- front/src/stores/constants.ts
+++ front/src/stores/constants.ts
@@ -2,5 +2,5 @@ import { readable, type Readable } from 'svelte/store';
 
 export const CONSTANTS: Readable<{ [index: string]: string }> = readable({
 	AMADEUS_BLUE: 'rgb(0,94,184)',
-	API_URL: `http://${import.meta.env.VITE_EC2_INSTANCE_IP}`
+	API_URL: `http://localhost:3000`
 });
